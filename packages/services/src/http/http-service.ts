83a9a26ce9fd0beb280f7c4e3097f235a1006543
Thiago Mota <thiagomotasilva2000@gmail.com>
2026-02-21 18:15:53 -0300

    feat(packages/services): add services package

---
+++ packages/services/src/http/http-service.ts
@@ -0,0 +1,50 @@
+export type HTTPServiceResponse<T> =
+  | { data: T; error?: never }
+  | { data?: never; error: string }
+
+export class HTTPService {
+  constructor(private baseURL: string = '') { }
+
+  private async request<T>(url: string, method: string, body?: unknown): Promise<HTTPServiceResponse<T>> {
+    const res = await fetch(`${this.baseURL}${url}`, {
+      method,
+      headers: { "Content-Type": "application/json" },
+      body: body ? JSON.stringify(body) : undefined,
+    });
+
+    if (!res.ok) {
+      const error = await res.text();
+      return { error };
+    }
+
+    const contentType = res.headers.get("content-type");
+    const isJson = contentType?.includes("application/json");
+
+    if (isJson) {
+      const data = await res.json();
+      return { data };
+    }
+
+    return { error: "Unexpected response format" };
+  }
+
+  get<T>(url: string) {
+    return this.request<T>(url, "GET");
+  }
+
+  post<T>(url: string, body?: unknown) {
+    return this.request<T>(url, "POST", body);
+  }
+
+  patch<T>(url: string, body: unknown) {
+    return this.request<T>(url, "PATCH", body);
+  }
+
+  put<T>(url: string, body: unknown) {
+    return this.request<T>(url, "PUT", body);
+  }
+
+  delete<T>(url: string) {
+    return this.request<T>(url, "DELETE");
+  }
+}
